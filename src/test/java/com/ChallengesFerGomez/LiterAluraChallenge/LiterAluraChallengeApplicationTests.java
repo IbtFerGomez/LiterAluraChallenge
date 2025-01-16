979c3774a7eaf86a3ca8129a4fcfb7c89ac40c5f
Ibt.FerGómez <programacion.fernandogomez@gmail.com>
2025-01-15 23:26:48 -0600

    Inicial

---
+++ src/test/java/com/ChallengesFerGomez/LiterAluraChallenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChallengesFerGomez.LiterAluraChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
